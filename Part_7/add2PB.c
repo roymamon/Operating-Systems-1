36f4ce7846f588699e2c26c64be8168e98e2ba9f
roymamon <mamon8521@gmail.com>
2025-04-11 16:24:51 +0000

    finished add2PB

---
+++ Part_7/add2PB.c
@@ -0,0 +1,37 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define BUF_SIZE 1024
+#define PHONEBOOK_FILE "phones.txt"
+
+int main() {
+    char buffer[BUF_SIZE];
+    ssize_t bytes_read;
+
+    // Read from standard input
+    bytes_read = read(STDIN_FILENO, buffer, BUF_SIZE);
+    if (bytes_read <= 0) {
+        write(STDERR_FILENO, "Failed to read input\n", 23);
+        return 1;
+    }
+
+    // Open phonebook file for appending (create if doesn't exist)
+    int fd = open(PHONEBOOK_FILE, O_WRONLY | O_CREAT | O_APPEND, 0644);
+    if (fd < 0) {
+        write(STDERR_FILENO, "Failed to open phonebook file\n", 31);
+        return 1;
+    }
+
+    // Write input to file
+    ssize_t written = write(fd, buffer, bytes_read);
+    if (written != bytes_read) {
+        write(STDERR_FILENO, "Failed to write to phonebook file\n", 35);
+        close(fd);
+        return 1;
+    }
+
+    close(fd);
+    return 0;
+}
