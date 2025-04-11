3f0d9716656f60684ce311f24e64f8a136d5abad
roymamon <mamon8521@gmail.com>
2025-04-11 16:29:39 +0000

    initiated find phone

--- Part_7/add2PB.c
+++ Part_7/add2PB.c
@@ -10,21 +10,21 @@ int main() {
     char buffer[BUF_SIZE];
     ssize_t bytes_read;
 
-    // Read from standard input
+    //read from standard input
     bytes_read = read(STDIN_FILENO, buffer, BUF_SIZE);
     if (bytes_read <= 0) {
         write(STDERR_FILENO, "Failed to read input\n", 23);
         return 1;
     }
 
-    // Open phonebook file for appending (create if doesn't exist)
+    //open phonebook file for appending (create if doesn't exist)
     int fd = open(PHONEBOOK_FILE, O_WRONLY | O_CREAT | O_APPEND, 0644);
     if (fd < 0) {
         write(STDERR_FILENO, "Failed to open phonebook file\n", 31);
         return 1;
     }
 
-    // Write input to file
+    //write input to file
     ssize_t written = write(fd, buffer, bytes_read);
     if (written != bytes_read) {
         write(STDERR_FILENO, "Failed to write to phonebook file\n", 35);
